62f792cabc00a1fc1bf1bc197f3fadc8c9971a88
Daniil <dgilev75@gmail.com>
2024-07-03 02:25:23 +0300

    fix: reorganize files into two repos

--- JS_CORE_ADVANCED/data_types.js
+++
@@ -1,8 +0,0 @@
-/**
- *? Напишите функцию, которая принимает строку и выводит в консоль последний символ строки
- */
-
-function lastChar(str) {
-  return str[str.length - 1];
-}
-console.log(lastChar("abcddg"));
